28539a120086db7279a3e5d6ffb093035d042107
김혜인 <haileyhi14@gmail.com>
2021-06-11 16:50:34 +0900

    list activity

--- app/src/main/java/com/test/medicineproject/MainActivity.java
+++ app/src/main/java/com/test/medicineproject/MainActivity.java
@@ -40,15 +40,17 @@ public class MainActivity extends AppCompatActivity {
 
         // 리사이클러뷰에 표시할 데이터 리스트 생성
         medicineList = new ArrayList<>();
-        //임시 데이터 넣기.
-        medicineList.add(new MedicineData("타이레놀", "얀센", "http://nedrug.mfds.go.kr/pbp/cmn/itemImageDownload/147426411393800107"));
-        medicineList.add(new MedicineData("탁센", "가나다", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7TVi20iFw3BOZAeb3XB2M1er8EuFbzrFL7A&usqp=CAU"));
+
+//        medicineList.add(new MedicineData("타이레놀", "얀센", "http://nedrug.mfds.go.kr/pbp/cmn/itemImageDownload/147426411393800107"));
+//        medicineList.add(new MedicineData("탁센", "가나다", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7TVi20iFw3BOZAeb3XB2M1er8EuFbzrFL7A&usqp=CAU"));
 
         // 리사이클러뷰에 adapter 적용
         recyclerView = (RecyclerView) findViewById(R.id.rv_medicine_list);
         adapter = new MedicineListAdapter(medicineList);
         recyclerView.setAdapter(adapter);
 
+        //임시 데이터 넣기.
+        loadData();
         searchText = (EditText) findViewById(R.id.et_search);
 
         //버튼 클릭 리스너 적용하기
@@ -58,12 +60,12 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Log.d(">>> onclick 확인 ", "검색 버튼 눌림!");
-                String keyword = searchText.getText().toString();
+                final String keyword = searchText.getText().toString();
                 new Thread(){
                     @Override
                     public void run() {
                         medicineList.clear();
-                        String urlAddress = address + "?serviceKey=" + key + "&numOfRows=10" + "&type=json";
+                        String urlAddress = address + "?serviceKey=" + key + "&itemName=" + keyword + "&numOfRows=10" + "&type=json";
                         try {
                             URL url = new URL(urlAddress);
 
@@ -95,7 +97,9 @@ public class MainActivity extends AppCompatActivity {
                                 String medicineName = temp.getString("itemName");
                                 String medicineCompany = temp.getString("entpName");
                                 String medicineImage = temp.getString("itemImage");
-                                medicineList.add(new MedicineData(medicineName, medicineCompany, medicineImage));
+
+//                                if(medicineName.contains(searchText.toString()))
+                                 medicineList.add(new MedicineData(medicineName, medicineCompany, medicineImage));
                             }
 
                             runOnUiThread(new Runnable() {
@@ -115,10 +119,72 @@ public class MainActivity extends AppCompatActivity {
                         }
 
                     }
-                };
+                }.start();
                 adapter.notifyDataSetChanged();
             }
         });
+    }
+
+    private void loadData(){
+        new Thread(){
+            @Override
+            public void run() {
+                medicineList.clear();
+                String urlAddress = address + "?serviceKey=" + key + "&numOfRows=30" + "&type=json";
+                try {
+                    URL url = new URL(urlAddress);
+
+                    InputStream is = url.openStream();
+                    InputStreamReader isr = new InputStreamReader(is);
+                    BufferedReader reader = new BufferedReader(isr);
+
+                    StringBuffer buffer = new StringBuffer();
+                    String line = reader.readLine();
+                    while (line != null) {
+                        buffer.append(line + "\n");
+                        line = reader.readLine();
+                    }
+
+                    String jsonData = buffer.toString();
+
+                    Gson gson = new Gson();
+                    //jsonData를 JSONObject로
+                    JSONObject obj = new JSONObject(jsonData);
+                    //obj의 items를 JSONObject로 추출
+                    JSONObject body = (JSONObject) obj.get("body");
+                    // itemResult의 JSONObject에서 JSONArray추출
+                    JSONArray itemsList = (JSONArray) body.get("items");
+
+                    Log.d("data 확인", ((JSONObject) itemsList.get(0)).getString("entpName"));
+                    for (int i = 0; i < itemsList.length(); i++) {
+                        JSONObject temp = itemsList.getJSONObject(i);
+
+                        String medicineName = temp.getString("itemName");
+                        String medicineCompany = temp.getString("entpName");
+                        String medicineImage = temp.getString("itemImage");
+
+                        medicineList.add(new MedicineData(medicineName, medicineCompany, medicineImage));
+                    }
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            adapter.notifyDataSetChanged();
+                        }
+                    });
+
+                } catch (MalformedURLException e) {
+                    Log.d("URL 문제", "URL이 잘못 되었다.");
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }.start();
+        adapter.notifyDataSetChanged();
     }
 
 
